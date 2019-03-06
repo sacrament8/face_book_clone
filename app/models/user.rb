8ed22c6b91c8ee75ee3d4e986da43573fa0c4de5
sacrament <xkinked.tailx@gmail.com>
2019-03-06 10:49:17 +0900

    userモデルにてバリデーション前にアドレスを強制的に小文字に変換するコールバックを追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_validation { email.downcase! }
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
