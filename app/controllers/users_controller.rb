6a0e35e3dec214e16d625aa6bfc82dfbfc7aed4e
sacrament <xkinked.tailx@gmail.com>
2019-03-06 11:48:18 +0900

    sign up機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      flash[:success] = 'ログイン処理に成功しました'
+      redirect_to user_path(@user.id)
     else
       render 'new'
     end
-    
+  end
+  def show
+    @user = User.find(params[:id])
   end
   private
   def user_params
