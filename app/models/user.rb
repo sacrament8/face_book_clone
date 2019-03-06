f7a29c79aca869cd3d789725cdcfdcebf0204e5c
sacrament <xkinked.tailx@gmail.com>
2019-03-06 10:56:23 +0900

    userモデルにpresence: trueと6文字以上のバリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
